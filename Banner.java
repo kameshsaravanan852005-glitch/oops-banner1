c18e0550de06f5d81725589442bc92cc71ff1637
kameshsaravanan852005-glitch <kameshsaravanan852005@gmail.com>
2026-02-28 23:00:24 +0530

    UC5: Combine array declaration and initialization using String.join()

--- Banner.java
+++ Banner.java
@@ -1,13 +1,3 @@
-/**
- * OOPS Banner Application
- *
- * Prints the word OOPS as a banner using '*' characters.
- *
- * UC4: Store banner lines in a String array and print using a loop.
- *
- * @author kameshsaravanan852005-glitch
- * @version 1.3 (UC4 - Array & Loop refactor)
- */
 public class Banner {
 
     public static void main(String[] args) {
