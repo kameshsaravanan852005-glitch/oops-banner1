31ea2e5838454ce7891d87d744c2da8d22017395
kameshsaravanan852005-glitch <kameshsaravanan852005@gmail.com>
2026-02-28 22:52:37 +0530

    UC4: Store OOPS banner in String array and print using loop

--- Banner.java
+++ Banner.java
@@ -1,14 +1,29 @@
+/**
+ * OOPS Banner Application
+ *
+ * Prints the word OOPS as a banner using '*' characters.
+ *
+ * UC4: Store banner lines in a String array and print using a loop.
+ *
+ * @author kameshsaravanan852005-glitch
+ * @version 1.3 (UC4 - Array & Loop refactor)
+ */
 public class Banner {
 
     public static void main(String[] args) {
 
-        System.out.println("  *****    *****    *****     ***** ");
-        System.out.println(" *     *  *     *  *     *   *       ");
-        System.out.println(" *     *  *     *  *     *   *       ");
-        System.out.println(" *     *  *     *  *****     *****  ");
-        System.out.println(" *     *  *     *  *              * ");
-        System.out.println(" *     *  *     *  *              *");
-        System.out.println("  *****    *****   *        *****");
+        String[] bannerLines = new String[] {
+            String.join("", " *****  ", " *****  ", " *****  ", " ***** "),
+            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
+            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
+            String.join("", "*     * ", "*     * ", " *****  ", " ***** "),
+            String.join("", "*     * ", "*     * ", " *      ", "      *"),
+            String.join("", "*     * ", "*     * ", " *      ", "      *"),
+            String.join("", " *****  ", " *****  ", " *    ",    "   ***** ")
+        };
 
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
 }
