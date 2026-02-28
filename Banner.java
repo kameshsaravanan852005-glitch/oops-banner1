66cde4dcaea043da8875102a3dcce1eaa8e0603e
kameshsaravanan852005-glitch <kameshsaravanan852005@gmail.com>
2026-02-28 22:42:25 +0530

    Update Banner.java

--- Banner.java
+++ Banner.java
@@ -1,14 +1,41 @@
+/**
+ * OOPS Banner Application
+ *
+ * Prints the word OOPS as a banner using '*' characters.
+ *
+ * @author kameshsaravanan852005-glitch
+ * @version 1.2 (UC3 - String.join refactor)
+ */
 public class Banner {
 
     public static void main(String[] args) {
 
-        System.out.println("  *****    *****    *****     ***** ");
-        System.out.println(" *     *  *     *  *     *   *       ");
-        System.out.println(" *     *  *     *  *     *   *       ");
-        System.out.println(" *     *  *     *  *****     *****  ");
-        System.out.println(" *     *  *     *  *              * ");
-        System.out.println(" *     *  *     *  *              *");
-        System.out.println("  *****    *****   *        *****");
+        System.out.println(String.join("", 
+                " *****  ", " *****  ", " *****  ", " ***** "
+        ));
 
+        System.out.println(String.join("", 
+                "*     * ", "*     * ", "*     * ", "*      "
+        ));
+
+        System.out.println(String.join("", 
+                "*     * ", "*     * ", "*     * ", "*      "
+        ));
+
+        System.out.println(String.join("", 
+                "*     * ", "*     * ", " *****  ", " ***** "
+        ));
+
+        System.out.println(String.join("", 
+                "*     * ", "*     * ", "      * ", "      *"
+        ));
+
+        System.out.println(String.join("", 
+                "*     * ", "*     * ", "      * ", "      *"
+        ));
+
+        System.out.println(String.join("", 
+                " *****  ", " *****  ", " *****  ", " ***** "
+        ));
     }
 }
