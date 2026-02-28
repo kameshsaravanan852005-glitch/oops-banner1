fa9b74956b442e99644dd77a136a886d39a8e023
kameshsaravanan852005-glitch <kameshsaravanan852005@gmail.com>
2026-02-28 22:57:44 +0530

    Update Banner.java

--- Banner.java
+++ Banner.java
@@ -1,29 +1,19 @@
-/**
- * OOPS Banner Application
- *
- * Prints the word OOPS as a banner using '*' characters.
- *
- * UC4: Store banner lines in a String array and print using a loop.
- *
- * @author kameshsaravanan852005-glitch
- * @version 1.3 (UC4 - Array & Loop refactor)
- */
-public class Banner {
-
+public class Banner{
     public static void main(String[] args) {
 
-        String[] bannerLines = new String[] {
-            String.join("", " *****  ", " *****  ", " *****  ", " ***** "),
-            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
-            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
-            String.join("", "*     * ", "*     * ", " *****  ", " ***** "),
-            String.join("", "*     * ", "*     * ", " *      ", "      *"),
-            String.join("", "*     * ", "*     * ", " *      ", "      *"),
-            String.join("", " *****  ", " *****  ", " *    ",    "   ***** ")
-        };
+        String[] bannerLines = new String[7];
+
+        bannerLines[0] = String.join("", "  *       *      ", "**      ", "**");
+        bannerLines[1] = String.join("", " *   *     *   *     ", "*   *    ", "*");
+        bannerLines[2] = String.join("", "*     *   *     *    ", "**      ", "**");
+        bannerLines[3] = String.join("", "*     *   *     *    ", "*            ", "*");
+        bannerLines[4] = String.join("", "*     *   *     *    ", "*            ", "*");
+        bannerLines[5] = String.join("", " *   *     *   *     ", "*        ", "*   *");
+        bannerLines[6] = String.join("", "  *       *      ", "*         ", "*");
 
         for (String line : bannerLines) {
             System.out.println(line);
+
         }
     }
 }
